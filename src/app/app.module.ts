27cb2deef5220450381f8d90f5a827b2d82f1cf0
AlexKitov <alkitov@gmail.com>
2018-02-13 21:53:34 +0100

    Add the AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service'; // <-- NgModel lives here
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module'; // <-- NgModel lives here
 
 @NgModule({
     declarations: [
@@ -19,7 +20,8 @@ import { MessageService } from './message.service'; // <-- NgModel lives here
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        AppRoutingModule
     ],
     providers: [
         HeroService,
