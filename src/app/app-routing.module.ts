1aca27444508798d3575b79a5b5bdedad1476e67
AlexKitov <alkitov@gmail.com>
2018-02-14 13:07:38 +0100

    RouterModule.forRoot()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {path: 'heroes', component: HeroesComponent}
+    { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
